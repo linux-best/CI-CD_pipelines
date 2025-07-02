9d725bb277d1f6510bea93da0c7a1af18bf578c5
jenkins <amirmahdifhp@gmail.com>
2025-07-02 04:51:43 -0400

    Update ,from loguru import logger

--- project/hello.py
+++ project/hello.py
@@ -1,8 +1,14 @@
 import sys
-
 from loguru import logger
+
+path="/var/lib/jenkins/logs/pipeline.log"
+
+logger.add(path ,
+           format="<yellow>{time: MMMM D, YYYY - HH:mm:ss}</yellow> -- <green>{level}</green> -- <level>{message}</level> {extra}",
+           level="DEBUG"
+)
 
 a = sys.argv[1]
 
-if a == "info" :
-    print(f"log_msg=> {sys.argv[1]}")
+if a == "amir" :
+    logger.info("log")
