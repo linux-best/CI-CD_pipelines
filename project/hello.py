0348b048e00d1befeef5a67ee50210833022f84e
jenkins <amirmahdifhp@gmail.com>
2025-07-02 04:48:57 -0400

    Update ,hello.py

--- project/hello.py
+++ project/hello.py
@@ -2,13 +2,6 @@ import sys
 
 from loguru import logger
 
-path="/var/lib/jenkins/logs/pipeline.log"
-
-logger.add(path ,
-           format="<yellow>{time: MMMM D, YYYY - HH:mm:ss}</yellow> -- <green>{level}</green> -- <level>{message}</level> {extra}",
-           level="DEBUG"
-)
-
 a = sys.argv[1]
 
 if a == "info" :
