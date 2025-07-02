c8f16b632dfc04efc41dd920048f65d0cb0ad14e
jenkins <amirmahdifhp@gmail.com>
2025-07-02 06:06:49 -0400

    Update ,project/

--- project/gro.groovy
+++ project/gro.groovy
@@ -1,12 +1,6 @@
-def log(x ,y ,z) {
+def logmsg(x,y,z) {
     sh """
-    sudo python3 project/log_msg.py ${x} ${y} ${z}
-    """
-}
-
-def logmsg(n,m) {
-    sh """
-    sudo python3 project/hello.py ${n} "${m}"
+    sudo python3 project/hello.py ${x} "${y}" "${z}"
     """
 }
 
--- project/hello.py
+++ project/hello.py
@@ -1,3 +1,4 @@
+# my_script.py
 import sys
 from loguru import logger
 
@@ -11,6 +12,14 @@ logger.add(path ,
 a = sys.argv[1]
 
 if a == "info" :
-    logger.info(f"{sys.argv[2]}")
+    with logger.contextualize(stage=sys.argv[3]):
+    	logger.info(f"{sys.argv[2]}")
 elif a == "success" :
-    logger.success(f"{sys.argv[2]}")
+    with logger.contextualize(stage=sys.argv[3]):
+	    logger.success(f"{sys.argv[2]}")
+
+
+#print(f"Script name: {sys.argv[0]}") # [0] is always the anme of the script
+#if len(sys.argv) > 1:
+#    print(f"Argument 1: {sys.argv[1]}") # first argument
+#    print(f"Argument 2: {sys.argv[2]}") # second argument
